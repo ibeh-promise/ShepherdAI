216123042b7d56b0749ffcea2a01fff9658a6ffe
ibeh-promise <ibehpromise3d@gmail.com>
2025-08-21 03:34:41 +0100

    setting up project working tree

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 bg-red-500 items-center justify-center"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
